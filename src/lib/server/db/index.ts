bafb572baf84b6b45ef2f9bd3b8685061787c93e
d3rpp <hudson.curren@icloud.com>
2025-04-26 11:52:15 +1200

    prevent badly attempted prerending

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,8 +6,8 @@ import * as schema from './schema';
 
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL && !building) throw new Error('DATABASE_URL is not set');
-if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+if (!building && !env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!dev && !building && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 const client = createClient({
 	url: env.DATABASE_URL,
