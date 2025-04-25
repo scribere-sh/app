6f62e58e6288940eb8ca19fd39a3d9c727eaa35b
d3rpp <hudson.curren@icloud.com>
2025-04-26 11:57:45 +1200

    STOP FUCKING CONNECTING TO THE DATABSE DURING A BUILD CUNT

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -7,11 +7,16 @@ import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!building && !env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && !building && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+if (!dev && !building && !env.DATABASE_AUTH_TOKEN)
+	throw new Error('DATABASE_AUTH_TOKEN is not set');
 
-const client = createClient({
-	url: env.DATABASE_URL,
-	authToken: env.DATABASE_AUTH_TOKEN
-});
+const client = (
+	!building
+		? createClient({
+				url: env.DATABASE_URL,
+				authToken: env.DATABASE_AUTH_TOKEN
+			})
+		: null
+)!;
 
-export const db = drizzle(client, { schema });
+export const db = (!building ? drizzle(client, { schema }) : null)!;
